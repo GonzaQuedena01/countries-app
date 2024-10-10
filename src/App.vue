cd3b5f3abd94f3331bcee71756a7f120f59a9e51
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-10 18:43:21 -0500

    feat(public): add toolbar component.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
+import Toolbar from './public/components/Toolbar.vue'
 
 </script>
 
 <template>
-
+  <Toolbar/>
 </template>
 
 <style scoped>
---
+++ src/public/components/Toolbar.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Toolbar"
+}
+</script>
+
+<template>
+  <div class="nav">
+    <span>Countries App</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
