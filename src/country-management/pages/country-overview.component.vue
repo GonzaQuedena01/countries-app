4e5a3cfac165c5cc918475dbd6574889c77baefc
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-11 01:42:03 -0500

    feat(country-management): add country catalog implementation in view.

--- src/country-management/pages/country-overview.component.vue
+++ src/country-management/pages/country-overview.component.vue
@@ -2,13 +2,27 @@
 import CountryService from "../services/country.service.js";
 import { onMounted, ref } from "vue";
 
+import CountryCatalog from "../components/country-catalog.component.vue";
+import Country from "../models/country.entity.js";
+
 const countries = ref([]);
 const apiCountries = new CountryService();
+
+const _buildCountryFromApiResponse = (response) => {
+  return response.map((country) => {
+    return new Country({
+      name: country.name.common,
+      capital: country.capital,
+      cca3: country.cca3,
+      flags: { alt: country.name.common, src: country.flags.png }
+    });
+  });
+}
 
 const _fetchCountries = async () => {
   try {
     const response = await apiCountries.fetchCountries();
-    countries.value = response.data;
+    countries.value = _buildCountryFromApiResponse(response.data);
     console.log(countries.value);
   }
   catch (error) {
@@ -23,7 +37,7 @@ onMounted(() => {
 </script>
 
 <template>
-
+  <country-catalog :countries="countries"/>
 </template>
 
 <style scoped>
