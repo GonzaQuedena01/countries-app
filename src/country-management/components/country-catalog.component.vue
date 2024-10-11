967e1562bc3a58a897a470d2ae36c182cc9f2526
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-11 01:41:02 -0500

    feat(country-management): add country catalog component.

--- src/country-management/components/country-catalog.component.vue
+++ src/country-management/components/country-catalog.component.vue
@@ -1,9 +1,24 @@
 <script setup>
+import CountryCard from "./country-card.component.vue";
+import { defineProps } from "vue";
+
+defineOptions({
+  name: 'country-catalog'
+});
+
+const props = defineProps({
+  countries: {
+    type: Array,
+    required: true
+  }
+});
 
 </script>
 
 <template>
-
+  <div class="catalog grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mt-20">
+    <country-card v-for="country in props.countries" :key="country.name" :country="country"/>
+  </div>
 </template>
 
 <style scoped>
