2653a3bc21d5eefa10a4c0b52a4ac1e40cea6af1
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-11 16:55:49 -0500

    feat(country-management): add country filter component.

---
+++ src/country-management/components/country-filter.component.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+defineOptions({
+  name: 'search-filter'
+});
+
+</script>
+
+<template>
+  <div class="mb-8">
+    <input type="text" class="border border-gray-300 rounded w-full p-1 px-4" placeholder="Search by country name"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
