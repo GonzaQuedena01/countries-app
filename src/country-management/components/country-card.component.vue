3edf0a92aa84f32c59dd15b3dd063b4a28e5c597
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-11 01:22:03 -0500

    feat(country-management): add country card component.

---
+++ src/country-management/components/country-card.component.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from "vue";
+import Country from "../models/country.entity.js";
+
+defineOptions({
+  name: 'country-card'
+});
+
+const props = defineProps({
+  country: {
+    type: Country,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+  <div class="border border-gray-300 rounded p-3">
+    {{ props.country.name}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
