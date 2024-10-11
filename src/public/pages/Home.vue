419cc8929806db0b28511fb9a8a705f0f50314c7
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-10 23:13:31 -0500

    feat(public): add home component resource.

---
+++ src/public/pages/Home.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <main>
+    <section class="home w-[100%]">
+      <div class="home__container w-[90%] mx-auto flex items-center mt-20 flex-col gap-10">
+
+        <h1 class="home__title text-4xl md:text-5xl lg:text-6xl text-center font-bold text-sky-700">
+          Welcome to Countries App
+        </h1>
+
+        <img src="https://cdn.pixabay.com/photo/2016/04/01/22/55/countries-1301799_1280.png" alt="world-map"
+          class="w-full h-auto min-w-[100px] max-w-[400px]"/>
+
+        <p class="home__text text-lg text-gray-500 text-center">This is a simple app that displays information about countries.</p>
+      </div>
+    </section>
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
