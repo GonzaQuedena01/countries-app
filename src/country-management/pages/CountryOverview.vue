02f16ffade23953ecab5bd25c597b3e96721f2ba
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-11 00:59:01 -0500

    chore: add access to country page.

---
+++ src/country-management/pages/CountryOverview.vue
@@ -0,0 +1,31 @@
+<script setup>
+import CountryService from "../services/country.service.js";
+import { onMounted, ref } from "vue";
+
+const countries = ref([]);
+const apiCountries = new CountryService();
+
+const _fetchCountries = async () => {
+  try {
+    const response = await apiCountries.fetchCountries();
+    countries.value = response.data;
+    console.log(countries.value);
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  _fetchCountries();
+});
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
