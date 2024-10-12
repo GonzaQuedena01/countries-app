65a12e72d3ff982b24bde3ba7b75a01101bfd156
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-12 01:35:45 -0500

    feat(country-management): implement country pagination.

--- src/country-management/pages/country-overview.component.vue
+++ src/country-management/pages/country-overview.component.vue
@@ -1,14 +1,17 @@
 <script setup>
 import CountryService from "../services/country.service.js";
-import { onMounted, ref, watch } from "vue";
+import { onMounted, ref, watch, computed } from "vue";
 
 import CountryCatalog from "../components/country-catalog.component.vue";
 import CountryFilter from "../components/country-filter.component.vue";
+import PaginatorButton from '../../shared/components/pagination-button.vue'
 import Country from "../models/country.entity.js";
 
 const countries = ref([]);
 const inputText = ref('');
 const filteredCountries = ref([]);
+const currentPage = ref(1);
+const itemsPerPage = ref(12);
 const apiCountries = new CountryService();
 
 const _buildCountryFromApiResponse = (response) => {
@@ -45,21 +48,41 @@ const _filterCountries = () => {
   });
 }
 
+const paginatedCountries = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage.value;
+  const end = start + itemsPerPage.value;
+  return filteredCountries.value.slice(start, end);
+});
+
+const previousPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+  }
+};
+
+const nextPage = () => {
+  if (currentPage.value * itemsPerPage.value < filteredCountries.value.length) {
+    currentPage.value++;
+  }
+};
+
 watch(inputText, _filterCountries);
 
 onMounted(() => {
   _fetchCountries();
 });
-
 </script>
 
 <template>
-  <div class="overview flex flex-col gap-4 mt-12">
+  <div class="overview flex flex-col gap-4 mt-12 mb-4">
     <country-filter @update-filter="_handleUpdate"/>
-    <country-catalog :countries="filteredCountries.length > 0 ? filteredCountries : countries"/>
+
+    <paginator-button :current-page="currentPage" :total-items="filteredCountries.length"
+                      :items-per-page="itemsPerPage" @previous="previousPage" @next="nextPage"/>
+
+    <country-catalog :countries="paginatedCountries"/>
   </div>
 </template>
 
 <style scoped>
-
 </style>
