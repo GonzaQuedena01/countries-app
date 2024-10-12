a2ac41820b4d4cd4a850b3dc6c29afca4240e1db
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-11 19:31:52 -0500

    feat(country-management): add country filter implementation.

--- src/country-management/components/country-filter.component.vue
+++ src/country-management/components/country-filter.component.vue
@@ -1,14 +1,23 @@
 <script setup>
+import { ref } from "vue";
 
 defineOptions({
   name: 'search-filter'
 });
 
+const countrySearched = ref('');
+const emit = defineEmits(['update-filter']);
+
+const handleInput = () => {
+  emit('update-filter', countrySearched.value);
+}
 </script>
 
 <template>
   <div class="mb-8">
-    <input type="text" class="border border-gray-300 rounded w-full p-1 px-4" placeholder="Search by country name"/>
+    <input type="text" class="border border-gray-300 rounded w-full p-1 px-4" placeholder="Search by country name"
+           v-model="countrySearched"
+            @input="handleInput"/>
   </div>
 </template>
 
--- src/country-management/pages/country-overview.component.vue
+++ src/country-management/pages/country-overview.component.vue
@@ -1,11 +1,14 @@
 <script setup>
 import CountryService from "../services/country.service.js";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 import CountryCatalog from "../components/country-catalog.component.vue";
+import CountryFilter from "../components/country-filter.component.vue";
 import Country from "../models/country.entity.js";
 
 const countries = ref([]);
+const inputText = ref('');
+const filteredCountries = ref([]);
 const apiCountries = new CountryService();
 
 const _buildCountryFromApiResponse = (response) => {
@@ -25,12 +28,24 @@ const _fetchCountries = async () => {
   try {
     const response = await apiCountries.fetchCountries();
     countries.value = _buildCountryFromApiResponse(response.data);
-    console.log(countries.value);
+    filteredCountries.value = countries.value;
   }
   catch (error) {
     console.error(error);
   }
 };
+
+const _handleUpdate = (countrySearched) => {
+  inputText.value = countrySearched;
+}
+
+const _filterCountries = () => {
+  filteredCountries.value = countries.value.filter((country) => {
+    return country.name.toLowerCase().includes(inputText.value.toLowerCase());
+  });
+}
+
+watch(inputText, _filterCountries);
 
 onMounted(() => {
   _fetchCountries();
@@ -39,7 +54,10 @@ onMounted(() => {
 </script>
 
 <template>
-  <country-catalog :countries="countries"/>
+  <div class="overview flex flex-col gap-4 mt-12">
+    <country-filter @update-filter="_handleUpdate"/>
+    <country-catalog :countries="filteredCountries.length > 0 ? filteredCountries : countries"/>
+  </div>
 </template>
 
 <style scoped>
